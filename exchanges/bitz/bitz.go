828f8b234655ccbad1e9861aa894872b233c9697
Serg Chernata <sergchernata@gmail.com>
2018-01-15 15:45:51 -0500

    basic bitz package converted form kucoin

---
+++ exchanges/bitz/bitz.go
@@ -0,0 +1,171 @@
+package bitz
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var api_url, api_key, api_secret string
+
+type Order struct {
+	Success bool `json:"success"`
+	Data    struct {
+		Id string `json:"orderOid"`
+	} `json:"data"`
+}
+
+type Holdings struct {
+	Holding Holding `json:"data"`
+	Success bool    `json:"success"`
+}
+
+type Holding struct {
+	Symbol string `json:"coinType"`
+	Amount string `json:"balanceStr,Number"`
+}
+
+type Prices struct {
+	Prices  []Price `json:"data"`
+	Success bool    `json:"success"`
+}
+
+type Price struct {
+	Symbol string      `json:"symbol"`
+	Price  json.Number `json:"lastDealPrice,Number"`
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func Initialize(url string, key string, secret string) {
+
+	fmt.Println("initializing kucoin package")
+
+	api_url = url
+	api_key = key
+	api_secret = secret
+
+}
+
+func Get_balances(tokens map[string]bool) map[string]string {
+
+	var holdings = make(map[string]string)
+	var body []byte
+
+	for token, _ := range tokens {
+
+		var data = new(Holdings)
+		var endpoint = "/v1/account/" + token + "/balance"
+		var params = ""
+
+		// perform api call
+		body = execute("GET", api_url, endpoint, params, true)
+
+		err := json.Unmarshal(body, &data)
+		check(err)
+
+		holdings[data.Holding.Symbol] = data.Holding.Amount
+
+	}
+
+	return holdings
+}
+
+func Get_price(tokens map[string]bool) map[string]string {
+
+	var params = ""
+	var endpoint = "/api_v1/tickerall"
+	var data = new(Prices)
+	var prices = make(map[string]string)
+	var body []byte
+
+	// perform api call
+	body = execute("GET", api_url, endpoint, params, false)
+
+	err := json.Unmarshal(body, &data)
+	check(err)
+
+	//parse data and format for return
+	for _, v := range data.Prices {
+
+		// kucoin formats pairs as "LINK-ETH"
+		// this will be the format we convert others to
+		symbol := v.Symbol
+		price := string(v.Price)
+		is_eth_pair := strings.HasSuffix(symbol, "_ETH")
+		token := strings.TrimSuffix(symbol, "_ETH")
+
+		if is_eth_pair && tokens[token] {
+			prices[symbol] = price
+		}
+	}
+
+	return prices
+}
+
+func Sell(token string, quantity int, price float64) (transaction_id string, sell_placed bool) {
+
+	token += "_ETH"
+	var timestamp = strconv.Itoa(int(time.Now().Unix() * 1000))
+	var params = fmt.Sprintf("api_key=%s&coin=%s&nonce=235195&number=%d&price=%f&timestamp=%d&tradepwd=%s&type=out&sign=%s", 
+		api_key, token, quantity, price, timestamp, spi_tradepwd)
+	var signature = make_signature(params)
+	var endpoint = "/api_v1/tradeAdd"
+	var order = new(Order)
+	var body []byte
+
+	params = params + "&sign=" + signature
+
+	// perform api call
+	body = execute("POST", api_url, endpoint, params, true)
+
+	err := json.Unmarshal(body, &order)
+	check(err)
+
+	if order.Data.Id == "" {
+		return "", false
+	}
+
+	return order.Data.Id, true
+
+}
+
+func make_signature(params string) signature string {
+
+	hasher := md5.New()
+    hasher.Write([]byte(params))
+    return hex.EncodeToString(hasher.Sum(nil))
+
+}
+
+func execute(method string, url string, endpoint string, params string, auth bool) []byte {
+
+	req, err := http.NewRequest(method, url+endpoint+"?"+params, nil)
+	check(err)
+
+	req.Header.Set("User-Agent", "test")
+	req.Header.Add("Accept", "application/json")
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	check(err)
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	check(err)
+
+	return body
+
+}
