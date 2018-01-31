2a7fca08ac29e04420e3e2ba0434a01f41b588dc
Serg Chernata <sergchernata@gmail.com>
2018-01-30 21:15:22 -0500

    discord package needs it's own connection to the database

--- discord/discord.go
+++ discord/discord.go
@@ -2,10 +2,13 @@ package discord
 
 import (
 	"fmt"
+	"strings"
+
 	// go get github.com/bwmarrin/discordgo
 	"github.com/bwmarrin/discordgo"
 
-	"strings"
+	// database package
+	"../db/mongo"
 
 	// utility
 	"../utils"
@@ -15,14 +18,23 @@ var auth_token, bot_id, channel_id string
 
 var session *discordgo.Session
 
-func Initialize(discord_auth_token, discord_bot_id, discord_channel_id string) {
+func Initialize(discord_auth_token, discord_bot_id, discord_channel_id,
+	mongo_host, mongo_database, mongo_username, mongo_password string) {
 
 	fmt.Println("initializing discord package")
 
 	auth_token = discord_auth_token
 	bot_id = discord_bot_id
 	channel_id = discord_channel_id
+	host = mongo_host
+	database = mongo_database
+	username = mongo_username
+	password = mongo_password
 
+	// initialize database connection
+	mongo.Initialize(host, database, username, password)
+
+	// initialize discord bot
 	session, err := discordgo.New("Bot " + auth_token)
 	utils.Check(err)
 
@@ -40,11 +52,14 @@ func Initialize(discord_auth_token, discord_bot_id, discord_channel_id string) {
 
 func message_handler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	//var messages []string
+	var messages []string
+	author_id := m.Author.ID
+	author_username := m.Author.Username
+	author_channel_id := m.ChannelID
 	content := strings.Replace(m.Content, " ", "", -1)
 
 	if content == "help" {
-		s.ChannelMessageSend(m.ChannelID, "helping you")
+		s.ChannelMessageSend(author_channel_id, "helping you")
 	}
 
 }
